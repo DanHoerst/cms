7ae21f20e3b53928db342cd517fc4696197ba62d
Danny830x <dhoerst1@gmail.com>
2012-01-29 20:14:41 -0500

    errors fixed

--- gambino/urls/categories.py
+++ gambino/urls/categories.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from gambino.models import Category
 
 # Category URLs
-urlpatterns = patterns('gambino.views',
+urlpatterns = patterns('',
     url(r'^$', 'django.views.generic.list_detail.object_list', { 'queryset': Category.objects.all() }),
     url(r'^(?P<slug>[-\w]+)/$', 'gambino.views.category_detail'),
 )
